c90d1d68ac9ebe780fdb6668cf29d2d76caa9f58
ashkanlarni <ash.larni@gmail.com>
2021-03-09 18:43:23 +0330

    "add" function added.

--- Session1/main.py
+++ Session1/main.py
@@ -1,3 +1,9 @@
+# Function to add two numbers
+def add(num1, num2):
+    temp = num1
+    return temp
+
+
 print("Please select operation -\n" \
       "1. Add\n" \
       "2. Subtract\n" \
@@ -9,3 +15,7 @@ select = int(input("Select operations form 1, 2, 3, 4 :"))
 
 number_1 = int(input("Enter first number: "))
 number_2 = int(input("Enter second number: "))
+
+if select == 1:
+    print(number_1, "+", number_2, "=",
+          add(number_1, number_2))
