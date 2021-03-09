150f50017af383e5a49df14aac2ed6603952589c
sfch1999 <sor100100@yahoo.com>
2021-03-09 18:44:40 +0330

    Subtract implemented

--- Session1/main.py
+++ Session1/main.py
@@ -1,5 +1,9 @@
 def add(num1, num2):
-      return num1 + num2
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
 
 
 print("Please select operation -\n" \
@@ -15,5 +19,8 @@ number_1 = int(input("Enter first number: "))
 number_2 = int(input("Enter second number: "))
 
 if select == 1:
-      print(number_1, "+", number_2, "=",
-            add(number_1, number_2))
+    print(number_1, "+", number_2, "=",
+          add(number_1, number_2))
+elif select == 2:
+    print(number_1, "-", number_2, "=",
+          subtract(number_1, number_2))
