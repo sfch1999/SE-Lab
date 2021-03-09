328c4f580d69bd9db6046901751c3943f4eb11e9
ashkanlarni <ash.larni@gmail.com>
2021-03-09 18:41:15 +0330

    Taking input from the user handled.

--- Session1/main.py
+++ Session1/main.py
@@ -1,1 +1,11 @@
-#TODO
+print("Please select operation -\n" \
+      "1. Add\n" \
+      "2. Subtract\n" \
+      "3. Multiply\n" \
+      "4. Divide\n")
+
+# Take input from the user
+select = int(input("Select operations form 1, 2, 3, 4 :"))
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
