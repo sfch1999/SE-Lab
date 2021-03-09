d3d28739baa010e89a79027d922f03a9ae4d7e18
ashkanlarni <ash.larni@gmail.com>
2021-03-09 18:44:44 +0330

    "multiply" function added.

--- Session1/main.py
+++ Session1/main.py
@@ -1,5 +1,10 @@
 def add(num1, num2):
-      return num1 + num2
+    return num1 + num2
+
+
+# Function to multiply two numbers
+def multiply(num1, num2):
+    return num1 * num2
 
 
 print("Please select operation -\n" \
@@ -15,5 +20,9 @@ number_1 = int(input("Enter first number: "))
 number_2 = int(input("Enter second number: "))
 
 if select == 1:
-      print(number_1, "+", number_2, "=",
-            add(number_1, number_2))
+    print(number_1, "+", number_2, "=",
+          add(number_1, number_2))
+
+elif select == 3:
+    print(number_1, "*", number_2, "=",
+          multiply(number_1, number_2))
